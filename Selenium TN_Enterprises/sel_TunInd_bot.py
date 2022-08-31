ef88af8f4fa3b7c4a1ce745b3a6efaa549c3df59
Spenzy <zieddr@gmail.com>
2022-09-01 00:05:37 +0100

    TnInd push

--- Selenium TN_Enterprises/sel_TunInd_bot.py
+++ Selenium TN_Enterprises/sel_TunInd_bot.py
@@ -17,39 +17,46 @@ select = Select(driver.find_element(By.NAME, 'secteur'))
 select.select_by_value("06")
 #Requesting search results
 driver.find_element(By.XPATH, "//input[@type='submit' and @value='Search']").click()
-contentdiv = driver.find_element(By.XPATH, "//div[@class='table-wrap'][2]")
-table = contentdiv.find_element(By.XPATH, "//table[@class='one']/tbody")
-tlist = table.find_elements(By.XPATH, "//tr[contains(@onclick,'document')]")
 
 #Init Json list
 data = {}
+data_nbr = 0
 
-#Iterate through table
-for nb in range(len(tlist)):
+for page in range(2, 18):
     contentdiv = driver.find_element(By.XPATH, "//div[@class='table-wrap'][2]")
     table = contentdiv.find_element(By.XPATH, "//table[@class='one']/tbody")
     tlist = table.find_elements(By.XPATH, "//tr[contains(@onclick,'document')]")
-    tlist[nb].click()
+    #Iterate through table
+    for nb in range(len(tlist)):
+        contentdiv = driver.find_element(By.XPATH, "//div[@class='table-wrap'][2]")
+        table = contentdiv.find_element(By.XPATH, "//table[@class='one']/tbody")
+        tlist = table.find_elements(By.XPATH, "//tr[contains(@onclick,'document')]")
+        tlist[nb].click()
 
-    #Scraping content table
-    contentdiv = driver.find_element(By.XPATH, "//div[@class='table-wrap']")
-    table = contentdiv.find_element(By.XPATH, "//table[@class='one']/tbody")
-    tcontent = table.find_elements(By.TAG_NAME, "tr")
-    #json data list init
-    content_json = {}
-    for row in tcontent:
-        first = row.find_element(By.CLASS_NAME, "first").text
-        last = row.find_element(By.CLASS_NAME, "last").text
-        print(first, last)
-        content_json[first] = last
-    data.append(content_json)
-    driver.back()
+        #Scraping content table
+        contentdiv = driver.find_element(By.XPATH, "//div[@class='table-wrap']")
+        table = contentdiv.find_element(By.XPATH, "//table[@class='one']/tbody")
+        tcontent = table.find_elements(By.TAG_NAME, "tr")
+        #json data list init
+        content_json = {}
+        for row in tcontent:
+            first = row.find_element(By.CLASS_NAME, "first").text
+            last = row.find_element(By.CLASS_NAME, "last").text
+            print(first, last)
+            content_json[first] = last
+        data[data_nbr] = content_json
+        data_nbr = data_nbr + 1
+        #Return to page
+        driver.back()
 
-print data
+#Next table
+    driver.find_element(By.XPATH, f"//*[contains(@href, 'dbs.asp?action=search&pagenum={page}')]").click()
 
-#timestr = time.strftime("%Y%m%d-%H%M%S")
+print (data)
+
+timestr = time.strftime("%Y%m%d-%H%M%S")
 # Serializing json
-#json_object = json.dumps(data, indent=4)
+json_object = json.dumps(data, indent=4)
  
 # Writing to sample.json
 #with open(f"Tunisie-Industrie-{timestr}.json", "w") as outfile:
